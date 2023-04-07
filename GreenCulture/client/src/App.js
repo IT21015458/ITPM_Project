e999321bfd803450f154dbaa78905ab54fb068e4
IT21015458 <it21015458@my.sliit.lk>
2023-04-07 07:37:24 +0530

    Sign In Form

--- GreenCulture/client/src/App.js
+++ GreenCulture/client/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+/*import './App.css';*/
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigationPage from './components/NavigationPage'
+import SignINForm from './components/SignINForm'
 
 function App() {
   return (
     <div>
-        <NavigationPage/>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<NavigationPage/>} />
+            <Route exact path="/signin" element={<SignINForm/>} /> 
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
